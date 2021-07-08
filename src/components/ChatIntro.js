8390f7afa938d7728a886857ee2b4b2da3057292
Joao Torquetti <joao.torquetti@outlook.ie>
2021-07-08 05:42:45 +0200

    first commit

---
+++ src/components/ChatIntro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChatIntro.css';
+
+export default() => {
+    return(
+        <div className="chatIntro">
+            <img src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg" alt=""></img> 
+            <h1>Keep your phone connected</h1>
+            <h2>WhatsApp connects to your phone to sync messages. To reduce data usage, connect your phoone to Wi-Fi</h2>
+        </div>
+    )
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+
+export default{
+    apiKey: "AIzaSyDh9vTWfnMn0GythQuQ8VTiRLSvxGuqwWg",
+    authDomain: "wp-clone-b32c9.firebaseapp.com",
+    projectId: "wp-clone-b32c9",
+    storageBucket: "wp-clone-b32c9.appspot.com",
+    messagingSenderId: "687170005403",
+    appId: "1:687170005403:web:97830ecf1b9652bb21d93e"
+  };
